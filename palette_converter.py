c1720e1f607f73c8e75723445c28b234ffe412b8
Luca Martinelli (linux) <lucxmangajet@gmail.com>
2022-07-24 00:09:59 +0200

    palette converter fixes. Converted palette to SNES colors

--- palette_converter.py
+++ palette_converter.py
@@ -1,5 +1,6 @@
 import argparse
 from ast import arg
+from cgi import test
 import io
 from argparse import ArgumentParser
 from sys import byteorder
@@ -9,12 +10,15 @@ from matplotlib import colors
 g_log = False
 args = None
 
-#0rrrrrgggggbbbbb
+#0bbbbbgggggrrrrr
 def rgb2snes(r,g, b):
     FILTER = 0x000000F8
     res = (b&FILTER) <<7
+    #print("{0:b}".format(res))
     res |= (g&FILTER) <<2
+    #print("{0:b}".format(res))
     res |= (r&FILTER) >>3
+    #print("{0:b}".format(res))
     return res
 
 def log(x):
@@ -27,23 +31,45 @@ def hex2snes(col):
     g = int(col[2:4], base=16)
     b = int(col[4:6], base=16)
     col = rgb2snes(r,g,b)
-    return col;
+    return col
+
+def snes2hex(color):
+    FILTER_RED =   0b0000000000011111
+    FILTER_GREEN = 0b0000001111100000
+    FILTER_BLUE =  0b0111110000000000
+    red =   (color&FILTER_RED)<<3
+    green = (color&FILTER_GREEN)>>2
+    blue =  (color&FILTER_BLUE)>>7 
+    col = "{:02x}{:02x}{:02x}".format(red, green, blue)
+    return col
+
     
 
 def test_hex2snes(vhex, expected):
     value = hex2snes(vhex)
     if expected != value:
-        print("TestError: {}. Expected: {}. Value: {}.".format(vhex, hex(expected), hex(value)))
+        print("hex2snes Error: {}. Expected: {}. Value: {}.".format(vhex, hex(expected), hex(value)))
 
+    chex = snes2hex(expected)
+    if int(chex, base=16) != int(vhex, base=16):
+        print("snes2hex Error: {}. Expected: {}. Value: {}.".format("{0:b}".format(expected), vhex, chex))
+
+    
+    
+    
 def run_unit_tests():
     log("Running unit tests")
     test_hex2snes("000000",0x00)
-    test_hex2snes("FFFFFF",0x7FFF)
+    test_hex2snes("F8F8F8",0x7FFF)
     #                      0b0bbbbbgggggrrrrr
-    test_hex2snes("FF0000",0b0000000000011111)
-    test_hex2snes("00FF00",0b0000001111100000)
-    test_hex2snes("0000FF",0b0111110000000000)
+    test_hex2snes("F80000",0b0000000000011111)
+    test_hex2snes("00F800",0b0000001111100000)
+    test_hex2snes("0000F8",0b0111110000000000)
     test_hex2snes("080808",0b0000010000100001)
+    test_hex2snes("C078B8",0x5df8)
+    test_hex2snes("d078b8",0b0101110111111010)
+
+
 
 
 def collect_colors():
@@ -64,11 +90,16 @@ def collect_colors():
             
             return colors
 
-def export_colors(colors):
+def export_colors_binary(colors):
     with open(args.outpath, mode = "wb") as f:
         for col in colors:
             f.write(col.to_bytes(2, byteorder='big'))
         
+
+def export_colors_hex(colors):
+    with open(args.outpath, mode = "w") as f:
+        for col in colors:
+            f.write("{}\n".format(snes2hex(col)))
 
 
 def run():
@@ -77,6 +108,7 @@ def run():
     parser.add_argument("outpath")
     parser.add_argument("--log", action ="store_true")
     parser.add_argument("--tests", action ="store_true")
+    parser.add_argument("--hex", action ="store_true")
 
     global args
     args = parser.parse_args()
@@ -87,13 +119,16 @@ def run():
     if(args.log):
         g_log = True
 
+
+
     if(args.tests):
         run_unit_tests()
 
     colors = collect_colors()
-    export_colors(colors)
-
-
+    if(args.hex):
+        export_colors_hex(colors)
+    else:
+        export_colors_binary(colors)
 
 
 
